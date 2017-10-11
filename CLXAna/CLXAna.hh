c698ce82bd7cf6bb40f79638303958d431e75dd7
user2 <lpgaff@gmail.com>
2017-10-11 18:22:24 +0200

    add missing header file

---
+++ CLXAna/CLXAna.hh
@@ -0,0 +1,17 @@
+// TreeAna: The main routine that calls g_clx
+// Liam Gaffney (liam.gaffney@cern.ch) - 01/05/2017
+
+#ifndef g_clx_hh
+# include "g_clx.hh"
+#endif
+
+#include "CommandLineInterface.hh"
+
+#include <TEnv.h>
+#include <TChain.h>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
