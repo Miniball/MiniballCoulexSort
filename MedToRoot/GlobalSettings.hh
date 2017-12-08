35da106dbff3ecd8c052d2f7ddf4ce09399986c1
user2 <lpgaff@gmail.com>
2017-12-08 12:25:51 +0100

    Clean up and add first event option

--- MedToRoot/GlobalSettings.hh
+++ MedToRoot/GlobalSettings.hh
@@ -16,393 +16,338 @@
 using namespace std;
 using namespace TMath;
 
-class GlobalSettings : public TObject
-{
+class GlobalSettings : public TObject {
+
 public:
-  GlobalSettings(){;};//default ctor
-  GlobalSettings( unsigned int, char* [] );
-  ~GlobalSettings();
 
-  friend ostream& operator <<(ostream &,const GlobalSettings &);
+	GlobalSettings(){};
+	GlobalSettings( unsigned int, char* [] );
+	~GlobalSettings();
 
-  inline unsigned short FirstMiniballDgf()
-  {
-    return fFirstMiniballDgf;
-  };
+	friend ostream& operator <<(ostream &,const GlobalSettings &);
 
-  inline unsigned short LastMiniballDgf()
-  {
-    return fLastMiniballDgf;
-  };
+	inline unsigned short FirstMiniballDgf() {
+		return fFirstMiniballDgf;
+	};
 
-  inline unsigned short BeamdumpDgf()
-  {
-    return fBeamdumpDgf;
-  };
+	inline unsigned short LastMiniballDgf() {
+		return fLastMiniballDgf;
+	};
 
-  inline unsigned short FirstAdc()
-  {
-    return fFirstAdc;
-  };
+	inline unsigned short BeamdumpDgf() {
+		return fBeamdumpDgf;
+	};
 
-  inline unsigned short LastAdc()
-  {
-    return fLastAdc;
-  };
+	inline unsigned short FirstAdc() {
+		return fFirstAdc;
+	};
 
-  inline unsigned short BraggChamber()
-  {
-    return fBraggChamber;
-  };
+	inline unsigned short LastAdc() {
+		return fLastAdc;
+	};
 
-  inline unsigned short Scaler()
-  {
-    return fScaler;
-  };
+	inline unsigned short BraggChamber() {
+		return fBraggChamber;
+	};
 
-  inline unsigned short PatternUnit()
-  {
-    return fPatternUnit;
-  };
+	inline unsigned short Scaler() {
+		return fScaler;
+	};
 
-  inline unsigned short FirstDgfScaler()
-  {
-    return fFirstDgfScaler;
-  };
+	inline unsigned short PatternUnit() {
+		return fPatternUnit;
+	};
 
-  inline unsigned short LastDgfScaler()
-  {
-    return fLastDgfScaler;
-  };
+	inline unsigned short FirstDgfScaler() {
+		return fFirstDgfScaler;
+	};
 
-  inline unsigned short NumberOfDgfModules()
-  {
-    return fNumberOfDgfModules;
-  };
+	inline unsigned short LastDgfScaler() {
+		return fLastDgfScaler;
+	};
 
-  inline unsigned short NumberOfAdcModules()
-  {
-    return fNumberOfAdcModules;
-  };
+	inline unsigned short NumberOfDgfModules() {
+		return fNumberOfDgfModules;
+	};
 
-  inline unsigned short NumberOfPatternUnits()
-  {
-    return fNumberOfPatternUnits;
-  };
+	inline unsigned short NumberOfAdcModules() {
+		return fNumberOfAdcModules;
+	};
 
-  inline unsigned short NumberOfDgfChannels()
-  {
-    return fNumberOfDgfChannels;
-  };
+	inline unsigned short NumberOfPatternUnits() {
+		return fNumberOfPatternUnits;
+	};
 
-  inline unsigned short NumberOfTimestampModules()
-  {
-    return fNumberOfTimestampModules;
-  };
+	inline unsigned short NumberOfDgfChannels() {
+		return fNumberOfDgfChannels;
+	};
 
-  inline unsigned short TimestampModule(unsigned short Index)
-  {
-    if(Index <= fNumberOfTimestampModules)
-      {
-	return fTimestampModule[Index]-fMarabouDgfModuleOffset;
-      }
-    else
-      {
-	return 0xffff;
-      }
-  };
+	inline unsigned short NumberOfTimestampModules() {
+		return fNumberOfTimestampModules;
+	};
 
-  inline double TimestampModuleOffset(unsigned short Index)
-  {
-    if(Index <= fNumberOfTimestampModules)
-      {
-	return fTimestampModuleOffset[Index];
-      }
-    else
-      {
-	return -1.;
-      }
-  };
+	inline unsigned short TimestampModule(unsigned short Index) {
+		if( Index <= fNumberOfTimestampModules )
+			return fTimestampModule[Index]-fMarabouDgfModuleOffset;
+		else
+			return 0xffff;
+	};
 
-  inline unsigned short TimestampChannel()
-  {
-    return fTimestampChannel;
-  };
+	inline double TimestampModuleOffset(unsigned short Index) {
+    	if( Index <= fNumberOfTimestampModules )
+			return fTimestampModuleOffset[Index];
+		else
+			return -1.;
+	};
 
-  inline unsigned short NumberOfAdcsPerTimestampModule()
-  {
-    return fNumberOfAdcsPerTimestampModule;
-  };
+	inline unsigned short TimestampChannel() {
+		return fTimestampChannel;
+	};
 
-  inline unsigned short EbisT1AndSuperCycleModule()
-  {
-    return fEbisT1AndSuperCycleModule-fMarabouDgfModuleOffset;
-  };
+	inline unsigned short NumberOfAdcsPerTimestampModule() {
+		return fNumberOfAdcsPerTimestampModule;
+	};
 
-  inline unsigned short EbisChannel()
-  {
-    return fEbisChannel;
-  };
+	inline unsigned short EbisT1AndSuperCycleModule() {
+		return fEbisT1AndSuperCycleModule-fMarabouDgfModuleOffset;
+	};
 
-  inline unsigned short T1Channel()
-  {
-    return fT1Channel;
-  };
+	inline unsigned short EbisChannel() {
+		return fEbisChannel;
+	};
 
-  inline unsigned short SuperCycleChannel()
-  {
-    return fSuperCycleChannel;
-  };
+	inline unsigned short T1Channel() {
+		return fT1Channel;
+	};
 
-  inline unsigned short MarabouDgfModuleOffset()
-  {
-    return fMarabouDgfModuleOffset;
-  };
+	inline unsigned short SuperCycleChannel() {
+		return fSuperCycleChannel;
+	};
 
-  inline unsigned short MarabouAdcModuleOffset()
-  {
-    return fMarabouAdcModuleOffset;
-  };
+	inline unsigned short MarabouDgfModuleOffset() {
+		return fMarabouDgfModuleOffset;
+	};
 
-  inline unsigned short MarabouPatternUnitOffset()
-  {
-    return fMarabouPatternUnitOffset;
-  };
+	inline unsigned short MarabouAdcModuleOffset() {
+		return fMarabouAdcModuleOffset;
+	};
 
-  inline long long LastEvent()
-  {
-    return fLastEvent;
-  };
+	inline unsigned short MarabouPatternUnitOffset() {
+		return fMarabouPatternUnitOffset;
+	};
 
-  inline int VerboseLevel()
-  {
-    return fVerboseLevel;
-  }
+	inline long long FirstEvent() {
+		return fFirstEvent;
+	};
 
-  inline const char* MedFile()
-  {
-    return fMedFile.c_str();
-  };
+	inline long long LastEvent() {
+		return fLastEvent;
+	};
+
+	inline int VerboseLevel() {
+		return fVerboseLevel;
+	};
+
+	inline const char* MedFile() {
+		return fMedFile.c_str();
+	};
   
-  inline const char* OnBeamFile()
-  {
-    return fOnBeamFile.c_str();
-  };
+	inline const char* OnBeamFile() {
+		return fOnBeamFile.c_str();
+	};
 
-  inline const char* OnBeamBackgroundFile()
-  {
-    return fOnBeamBackgroundFile.c_str();
-  };
+	inline const char* OnBeamBackgroundFile() {
+		return fOnBeamBackgroundFile.c_str();
+	};
 
-  inline const char* OffBeamFile()
-  {
-    return fOffBeamFile.c_str();
-  };
+	inline const char* OffBeamFile() {
+		return fOffBeamFile.c_str();
+	};
 
-  inline const char* ScalerFile()
-  {
-    return fScalerFile.c_str();
-  };
+	inline const char* ScalerFile() {
+		return fScalerFile.c_str();
+	};
 
-  bool IsTimestampModule(unsigned short ModuleNumber)
-  {
-    for(unsigned short i = 0;i < fNumberOfTimestampModules; i++)
-      {
-	if(ModuleNumber == fTimestampModule[i]-fMarabouDgfModuleOffset)
-	  return true;
-      }
+	bool IsTimestampModule(unsigned short ModuleNumber) {
 
-    if(ModuleNumber == fEbisT1AndSuperCycleModule-fMarabouDgfModuleOffset)
-      return true;;
+		for( unsigned short i = 0;i < fNumberOfTimestampModules; i++ )
+			if( ModuleNumber == fTimestampModule[i]-fMarabouDgfModuleOffset )
+				return true;
 
-    return false;
-  };
+		if( ModuleNumber == fEbisT1AndSuperCycleModule-fMarabouDgfModuleOffset )
+			return true;;
 
-  inline bool Coincident(long long time)
-  {
-    if(fLowestCoincidence < time && time < fHighestCoincidence)
-      {
-	return true;
-      }
-    return false;
-  };
+		return false;
 
-  unsigned short RealTimeIndex()
-  {
-    return fRealTimeIndex;
-  };
+	};
 
-  unsigned short RunTimeIndex()
-  {
-    return fRunTimeIndex;
-  };
+	inline bool Coincident( long long time ) {
+		if( fLowestCoincidence < time && time < fHighestCoincidence )
+			return true;
+		else
+			return false;
+	};
 
-  unsigned short GSLTTimeIndex()
-  {
-    return fGSLTTimeIndex;
-  };
+	unsigned short RealTimeIndex() {
+		return fRealTimeIndex;
+	};
 
-  unsigned short NumberOfEventsIndex()
-  {
-    return fNumberOfEventsIndex;
-  };
+	unsigned short RunTimeIndex() {
+		return fRunTimeIndex;
+	};
 
-  unsigned short ChannelIndicesOffset(unsigned short channel)
-  {
-    if(channel >= fNumberOfDgfChannels)
-      {
-	cerr<<"Error, channel "<<channel<<" out of range (0 - "<<fNumberOfDgfChannels<<")"<<endl;
+	unsigned short GSLTTimeIndex() {
+		return fGSLTTimeIndex;
+	};
 
-	return 0xffff;
-      }
+	unsigned short NumberOfEventsIndex() {
+		return fNumberOfEventsIndex;
+	};
 
-    return fChannelIndicesOffset[channel];
-  };
+	unsigned short ChannelIndicesOffset( unsigned short channel ) {
+		if( channel >= fNumberOfDgfChannels ) {
+			cerr << "Error, channel " << channel << " out of range (0 - " << fNumberOfDgfChannels << ")" << endl;
+			return 0xffff;
+		}
+		return fChannelIndicesOffset[channel];
+	};
 
-  unsigned short LiveTimeIndex()
-  {
-    return fLiveTimeIndex;
-  };
+	unsigned short LiveTimeIndex() {
+		return fLiveTimeIndex;
+	};
 
-  unsigned short FastPeakIndex()
-  {
-    return fFastPeakIndex;
-  };
+	unsigned short FastPeakIndex() {
+		return fFastPeakIndex;
+	};
 
-  unsigned short BraggTraceLength()
-  {
-    return fBraggTraceLength;
-  };
+	unsigned short BraggTraceLength() {
+		return fBraggTraceLength;
+	};
 
-  bool SourceRun()
-  {
-    return fSourceRun;
-  };
+	bool SourceRun() {
+		return fSourceRun;
+	};
 
-  bool IncludeScaler()
-  {
-    return fIncludeScaler;
-  };
+	bool IncludeScaler() {
+		return fIncludeScaler;
+	};
 
-  bool MesytecAdc()
-  {
-    return fMesytecAdc;
-  };
+	bool MesytecAdc() {
+		return fMesytecAdc;
+	};
   
-  bool SPEDEChamb()
-  {
-    return fSPEDEChamb;
-  };
+	bool SPEDEChamb() {
+		return fSPEDEChamb;
+	};
 
-  unsigned short NofCaenAdc()
-  {
-    return fNofCaenAdc;
-  };
+	unsigned short NofCaenAdc() {
+		return fNofCaenAdc;
+	};
 
-  unsigned short TypeOfSetup()
-  {
-    return fTypeOfSetup;
-  };
+	unsigned short TypeOfSetup() {
+		return fTypeOfSetup;
+	};
   
-  
-  unsigned short EbisWindowLowerEdge()
-  {
-    return fEbisWindowLowerEdge;
-  };
+	unsigned short EbisWindowLowerEdge() {
+		return fEbisWindowLowerEdge;
+	};
 
-  unsigned short EbisWindowUpperEdge()
-  {
-    return fEbisWindowUpperEdge;
-  };
-  long long DgfInitDelay(){
-    return fDgfInitDelay;
-  }
+	unsigned short EbisWindowUpperEdge() {
+		return fEbisWindowUpperEdge;
+	};
 
-  bool Verify(); //performs some consistency checks on the settings
+	long long DgfInitDelay() {
+		return fDgfInitDelay;
+	};
+
+	bool Verify(); // performs some consistency checks on the settings
 
 protected:
-  void ReadSettings();
-  void ReadUnpackerSettings();
+	void ReadSettings();
+	void ReadUnpackerSettings();
 
 protected:
-  string fMedFile;
-  string fOnBeamFile;
-  string fOnBeamBackgroundFile;
-  string fOffBeamFile;
-  string fScalerFile;
-  string fSettingsFile;
+	string fMedFile;
+	string fOnBeamFile;
+	string fOnBeamBackgroundFile;
+	string fOffBeamFile;
+	string fScalerFile;
+	string fSettingsFile;
 
-  TEnv* fSettings;
+	TEnv* fSettings;
 
-  int fVerboseLevel;
-  long long fLastEvent;
+	int fVerboseLevel;
+	long long fFirstEvent;
+	long long fLastEvent;
 
-  //Module numbers
-  unsigned short fFirstMiniballDgf;
-  unsigned short fLastMiniballDgf;
+	// Module numbers
+	unsigned short fFirstMiniballDgf;
+	unsigned short fLastMiniballDgf;
       
-  unsigned short fBeamdumpDgf;
+	unsigned short fBeamdumpDgf;
       
-  unsigned short fFirstAdc;
-  unsigned short fLastAdc;
+	unsigned short fFirstAdc;
+	unsigned short fLastAdc;
       
-  unsigned short fBraggChamber;
-  unsigned short fScaler;
-  unsigned short fPatternUnit;
-  unsigned short fFirstDgfScaler;
-  unsigned short fLastDgfScaler;
+	unsigned short fBraggChamber;
+	unsigned short fScaler;
+	unsigned short fPatternUnit;
+	unsigned short fFirstDgfScaler;
+	unsigned short fLastDgfScaler;
 
-  double fCoincidenceWindowWidth;
-  double fReferencePointOfCoincidenceWindow;
+	double fCoincidenceWindowWidth;
+	double fReferencePointOfCoincidenceWindow;
 
-  long long fLowestCoincidence;
-  long long fHighestCoincidence;
+	long long fLowestCoincidence;
+	long long fHighestCoincidence;
 
-  unsigned short fNumberOfDgfModules;
-  unsigned short fNumberOfAdcModules;
-  unsigned short fNumberOfPatternUnits;
+	unsigned short fNumberOfDgfModules;
+	unsigned short fNumberOfAdcModules;
+	unsigned short fNumberOfPatternUnits;
 
-  unsigned short fNumberOfDgfChannels;
+	unsigned short fNumberOfDgfChannels;
 
-  unsigned short fNumberOfAdcsPerTimestampModule;
+	unsigned short fNumberOfAdcsPerTimestampModule;
 
-  unsigned short fNumberOfTimestampModules;
-  unsigned short* fTimestampModule; //!fNumberOfTimestampModules
-  double* fTimestampModuleOffset;//!fNumberOfTimestampModules
+	unsigned short fNumberOfTimestampModules;
+	unsigned short* fTimestampModule; //!fNumberOfTimestampModules
+	double* fTimestampModuleOffset;//!fNumberOfTimestampModules
 
-  unsigned short fTimestampChannel;
+	unsigned short fTimestampChannel;
 
-  unsigned short fEbisT1AndSuperCycleModule;
-  unsigned short fEbisChannel;
-  unsigned short fT1Channel;
-  unsigned short fSuperCycleChannel;
+	unsigned short fEbisT1AndSuperCycleModule;
+	unsigned short fEbisChannel;
+	unsigned short fT1Channel;
+	unsigned short fSuperCycleChannel;
 
-  unsigned short fMarabouDgfModuleOffset;
-  unsigned short fMarabouAdcModuleOffset;
-  unsigned short fMarabouPatternUnitOffset;
+	unsigned short fMarabouDgfModuleOffset;
+	unsigned short fMarabouAdcModuleOffset;
+	unsigned short fMarabouPatternUnitOffset;
 
-  //dgf scaler parameter indices
-  unsigned short fRealTimeIndex;
-  unsigned short fRunTimeIndex;
-  unsigned short fGSLTTimeIndex;
-  unsigned short fNumberOfEventsIndex;
-  unsigned short* fChannelIndicesOffset;
-  unsigned short fLiveTimeIndex;
-  unsigned short fFastPeakIndex;
+	// dgf scaler parameter indices
+	unsigned short fRealTimeIndex;
+	unsigned short fRunTimeIndex;
+	unsigned short fGSLTTimeIndex;
+	unsigned short fNumberOfEventsIndex;
+	unsigned short* fChannelIndicesOffset;
+	unsigned short fLiveTimeIndex;
+	unsigned short fFastPeakIndex;
 
-  unsigned short fBraggTraceLength;
+	unsigned short fBraggTraceLength;
 
-  bool fSourceRun;
-  bool fIncludeScaler;
-  bool fMesytecAdc;
-  bool fSPEDEChamb;
-  unsigned short fNofCaenAdc;
-  unsigned short fTypeOfSetup;
-  unsigned short fEbisWindowLowerEdge;
-  unsigned short fEbisWindowUpperEdge;
+	bool fSourceRun;
+	bool fIncludeScaler;
+	bool fMesytecAdc;
+	bool fSPEDEChamb;
+	unsigned short fNofCaenAdc;
+	unsigned short fTypeOfSetup;
+	unsigned short fEbisWindowLowerEdge;
+	unsigned short fEbisWindowUpperEdge;
 
-  long long fDgfInitDelay;
-  ClassDef(GlobalSettings, 1)
+	long long fDgfInitDelay;
+
+	ClassDef( GlobalSettings, 1 )
+
 };
 
 #endif
--- MedToRoot/MedToRoot.cc
+++ MedToRoot/MedToRoot.cc
@@ -9,11 +9,13 @@
 using namespace std;
 
 extern "C" {
+
 	//unsigned int mbs_next_sheader(const MBSDataIO *);
 	//unsigned int mbs_next_sdata(const MBSDataIO *);
 	const MBSDataIO * mbs_open_file(const char *, const char *, int, FILE *);
 	int mbs_close_file(const MBSDataIO *);
 	unsigned int mbs_next_event(const MBSDataIO *);
+
 }
 
 int main(int argc, char* argv[]) {
@@ -36,7 +38,7 @@ int main(int argc, char* argv[]) {
 	// open med file  (works only from file !!!)
 	const MBSDataIO* mbs;
 	
-	printf("opening file %s ...\n", Settings->MedFile());
+	printf( "opening file %s ...\n", Settings->MedFile() );
 	
 	mbs = mbs_open_file(Settings->MedFile(), "F", 0x4000, NULL);
 	
@@ -44,60 +46,70 @@ int main(int argc, char* argv[]) {
 	if( mbs == NULL ) printf("couldn't open file %s -> exit!!!\n", Settings->MedFile());
 	
 	//EventBuilder instance
-	EventBuilder* eventBuilder = new EventBuilder(Settings);
+	EventBuilder* eventBuilder = new EventBuilder( Settings );
 	
 	// endless loop over events
-	while(mbs != NULL) {
+	while( mbs != NULL ) {
 		
 		// output every 1e4 readout events
-		if(nofProcessedEvents%10000==0)
-		{
+		if( nofProcessedEvents%10000 == 0 ) {
 
 			cout << "Processing event number " << setw(7) << nofProcessedEvents << "\r";
 			cout.flush();
 	
 		}
 		
+		// check #of processed events
+		if( nofProcessedEvents < Settings->FirstEvent() ) continue;
+		if( nofProcessedEvents == Settings->FirstEvent() ) {
+
+			cout << "Processing event number " << setw(7) << nofProcessedEvents << "\r";
+			cout.flush();
+	
+		}
+
 		// get med buffer and call med-hdsubs
 		unsigned int eventType = 0;
 		
-		eventType = mbs_next_event(mbs);	                          /* next event */
-		if(eventType == MBS_ETYPE_EOF)
-		{		                  /* end of file? */
-			printf("End of file %s\n", Settings->MedFile());
-			mbs_close_file(mbs);
+		eventType = mbs_next_event(mbs);	// next event 
+		if( eventType == MBS_ETYPE_EOF ) {	// end of file?
+
+			printf( "End of file %s\n", Settings->MedFile() );
+			mbs_close_file( mbs );
 			break;
+
 		}
-		else if(eventType == MBS_ETYPE_ERROR)
-		{	                  /* error? */
-			printf("Aborting after error\n");
+
+		else if( eventType == MBS_ETYPE_ERROR ) {	// error?
+
+			printf( "Aborting after error\n" );
 			break;
+
 		}
-		else if(eventType == MBS_ETYPE_ABORT)
-		{	                  /* abort? */
-			printf("Aborting\n");
+
+		else if( eventType == MBS_ETYPE_ABORT ) {	 // abort?
+
+			printf( "Aborting\n" );
 			break;
+
 		}
+
 		else {
-			status = eventBuilder->ProcessEvent(mbs);
+
+			status = eventBuilder->ProcessEvent( mbs );
+
 		}
 		
 		// check #of processed events
-		if(nofProcessedEvents == Settings->LastEvent())
-		{
-			break;
-		}
-		
-		//if stop trigger was found or an illegal trigger => stop
-		if(status == 15 || status == 2)
-		{
-			break;
-		}
-		
+		if( nofProcessedEvents == Settings->LastEvent() ) break;
+
+		// if stop trigger was found or an illegal trigger => stop
+		if( status == 15 || status == 2 ) break;
+
 		// increment number of processed events
 		nofProcessedEvents++;
 		
-	} //while(mbs != NULL)
+	} // while(mbs != NULL)
 	
 	cout << endl;
 	
@@ -107,6 +119,6 @@ int main(int argc, char* argv[]) {
 	
 	return 0;
 	
-}//end of main
+} //end of main
 
 
