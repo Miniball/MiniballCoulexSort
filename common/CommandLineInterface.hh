3d1d735fc587601cf241bdb0d911ed6a5121fc98
Liam Gaffney <lpgaff@gmail.com>
2020-10-26 17:28:03 +0000

    whistespace

--- common/CommandLineInterface.hh
+++ common/CommandLineInterface.hh
@@ -10,42 +10,43 @@
 
 using namespace std;
 
-class CommandLineInterface
-{
+class CommandLineInterface {
+	
 public:
-  CommandLineInterface();
-  ~CommandLineInterface(){};
-
-  //main functions to check all flags from command line
-  bool CheckFlags( unsigned int, char*[], const bool& Debug = false );
-
-  //functions to add flags
-  void Add(const char*);
-  void Add(const char*, const char*, bool*);
-  void Add(const char*, const char*, char**);
-  void Add(const char*, const char*, string*);
-  void Add(const char*, const char*, int*);
-  void Add(const char*, const char*, float*);
-  void Add(const char*, const char*, size_t*);
-  void Add(const char*, const char*, long long*);
-  void Add(const char*, const char*, double*, double factor = 1.);
-  void Add(const char*, const char*, vector<char*>*);
-  void Add(const char*, const char*, vector<string>*);
-  void Add(const char*, const char*, vector<int>*);
-  void Add(const char*, const char*, vector<long long>*);
-  void Add(const char*, const char*, vector<double>*, double factor = 1.);
-
-  friend ostream& operator <<(ostream &,const CommandLineInterface &);
-
+	CommandLineInterface();
+	~CommandLineInterface(){};
+	
+	//main functions to check all flags from command line
+	bool CheckFlags( unsigned int, char*[], const bool& Debug = false );
+	
+	//functions to add flags
+	void Add(const char*);
+	void Add(const char*, const char*, bool*);
+	void Add(const char*, const char*, char**);
+	void Add(const char*, const char*, string*);
+	void Add(const char*, const char*, int*);
+	void Add(const char*, const char*, float*);
+	void Add(const char*, const char*, size_t*);
+	void Add(const char*, const char*, long long*);
+	void Add(const char*, const char*, double*, double factor = 1.);
+	void Add(const char*, const char*, vector<char*>*);
+	void Add(const char*, const char*, vector<string>*);
+	void Add(const char*, const char*, vector<int>*);
+	void Add(const char*, const char*, vector<long long>*);
+	void Add(const char*, const char*, vector<double>*, double factor = 1.);
+	
+	friend ostream& operator <<(ostream &,const CommandLineInterface &);
+	
 private:
-  unsigned int fMaximumFlagLength;
-  vector<string> fFlags;
-  vector<void*>  fValues;
-  unsigned int fMaximumTypeLength;
-  vector<string> fTypes;
-  unsigned int fMaximumCommentLength;
-  vector<string> fComments;
-  vector<double> fFactors;
+	unsigned int fMaximumFlagLength;
+	vector<string> fFlags;
+	vector<void*>  fValues;
+	unsigned int fMaximumTypeLength;
+	vector<string> fTypes;
+	unsigned int fMaximumCommentLength;
+	vector<string> fComments;
+	vector<double> fFactors;
+	
 };
 
 #endif
